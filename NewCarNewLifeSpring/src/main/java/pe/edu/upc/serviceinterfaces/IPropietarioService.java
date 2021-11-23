03f4bc00bf5eb670ed2cf487a558c2aa7f1ea841
Alejandro2Vargas <U20181G899@upc.edu.pe>
2021-11-22 22:50:59 -0500

    voltron unido

---
+++ NewCarNewLifeSpring/src/main/java/pe/edu/upc/serviceinterfaces/IPropietarioService.java
@@ -0,0 +1,26 @@
+package pe.edu.upc.serviceinterfaces;
+
+import java.util.List;
+import java.util.Optional;
+
+import pe.edu.upc.entities.Propietario;
+
+public interface IPropietarioService {
+
+	public Integer insert(Propietario prop);
+
+	List<Propietario> list();
+
+	public void delete(long idProp);
+
+	Optional<Propietario> findById(Long idProp);
+
+	List<Propietario> fetchPropietarioByName(String nameProp);
+
+	public List<Propietario> fetchPropietarioByCategoryName(String nameCategory);
+
+	public List<Propietario> findByNamePropietarioLikeIgnoreCase(String nameProp);
+
+	public List<String[]> prodXord();
+
+}
